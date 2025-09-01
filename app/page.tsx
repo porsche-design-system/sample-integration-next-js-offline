4649b9de5434860eb70b024a50545f62e0fe8a45
Stephan Schröter <stephan.schroeter@me.com>
2025-09-01 17:42:29 +0200

    test: add e2e integrity checks | sas

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,18 @@ import Image from "next/image";
 import { PWordmark, PButton, PFlag } from '@porsche-design-system/components-react/ssr';
 
 export default function Home() {
+  console.log("Home page");
+  console.warn("Some warning");
+  console.error("Some error");
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div
+      className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <img src="https://cdn.ui.porsche.com/porsche-design-system/flags/de.b575e11.svg" width="24" height="24"
+           loading="lazy" alt="Flag of Germany"/>
       <div>
-        <PWordmark />
+        <PWordmark/>
         <PButton icon="add">Some label</PButton>
-        <PFlag />
+        <PFlag/>
       </div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
