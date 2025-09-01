0683536dd9edfffa325ecb3627fd4a91a961473b
Stephan Schröter <stephan.schroeter@me.com>
2025-09-01 18:40:26 +0200

    test: add e2e integrity checks | sas

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ export default function Home() {
   return (
     <div
       className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <img src="https://cdn.ui.porsche.com/porsche-design-system/flags/de.b575e11.svg" width="24" height="24"
-           loading="lazy" alt="Flag of Germany"/>
       <div>
         <PWordmark/>
         <PButton icon="add">Some label</PButton>
